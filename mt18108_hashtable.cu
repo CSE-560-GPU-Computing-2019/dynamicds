2e7231230bde311a81e7688bfefbdafd3802ddaa
sujay18108 <44543930+sujay18108@users.noreply.github.com>
2019-04-07 05:41:09 -0400

    Updated insertion of hashtable(LL part)

--- mt18108_hashtable.cu
+++ mt18108_hashtable.cu
@@ -2,118 +2,141 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_VAL 32
-#define MIN_VAL 0
-typedef struct Slab Slab;
+#include <stddef.h>
 typedef struct SlabList SlabList;
 
-//Slab is a linkedlist nodes having key value pair
-struct Slab {
-	Slab **next;
-  	int val;
-  	int key;
-};
-//List of ll nodes
-struct SlabList {
-	Slab *head;
+#define SLAB_SIZE 4
+struct SlabList{
+	int val[SLAB_SIZE];
+	int key[SLAB_SIZE];
+	struct SlabList* next;
 };
 
-SlabList *createSlablist(void);
-__device__ void insertSlablist(SlabList *slablist, int ele);
-//Method to search for the element befroe insertion
-__device__ Slab *node_search(SlabList *slablist, int ele, int search_key);
-//Method to create a new node
-__device__ Slab *node_create(int val, int key);
-//Method to create head node
-__global__ void create_head(SlabList *slablist);
-
-__global__ void create_head(SlabList *slablist){
-	//Call headnode creation with MAX permitted values
-	slablist->head = node_create(MIN_VAL, MAX_VAL);
-	memset(slablist->head->next, 0, MAX_VAL * sizeof(Slab *));
+struct SlabList* createSlablist(struct SlabList* head_ref, int* new_key, int* new_val, int size) { 
+	head_ref=NULL;
+	for(int i=0;i<size/4;i++){
+		struct SlabList* new_node = (struct SlabList*) malloc(sizeof(struct SlabList)); 
+		for(int j=0;j<SLAB_SIZE;j++){
+    			new_node->key[j] = new_key[i*SLAB_SIZE+j]; 
+			new_node->val[j]= new_val[i*SLAB_SIZE+j];
+	//		printf("key--->%d\tVal---->%d\n",new_node->key[j],new_node->val[j]);
+		} 
+    		new_node->next = head_ref; 
+     		head_ref    = new_node;
+	}
+	return head_ref;
+} 
+void printList(struct SlabList *node) { 
+    	while (node != NULL) { 
+		for(int i=0;i<SLAB_SIZE;i++){
+        		printf("Key: %d\tValue:%d\n",node->key[i],node->val[i]); 		
+		}
+  	     	node = node->next; 
+	} 
+} 
+void printList1(struct SlabList *node, int size) {
+	for(int j=0;j<size;j++) {
+		for(int i=0;i<SLAB_SIZE;i++){
+        		printf("Key: %d\tValue:%d\n",node[j].key[i],node[j].val[i]); 		
+		}
+	}
+} 
+__global__ void insertKernel(struct SlabList* head_ref, int* new_key, int* new_val, int size,struct SlabList* SL, struct SlabList* temp){
+//__global__ void insertKernel(struct SlabList* SL){
+	printf("INSIDEKERNEL!!!!\n");
+	int id = blockIdx.x*blockDim.x + threadIdx.x;
+	if(id==0){
+//SL->key[0]=1;
+//	SL->val[0]=2;
+//SL->next=NULL;
+	head_ref=NULL;
+	for(int i=0;i<size/SLAB_SIZE;i++){
+		//temp=NULL;
+		struct SlabList* new_node = (struct SlabList*) malloc(sizeof(struct SlabList)); 
+		for(int j=0;j<SLAB_SIZE;j++){
+    			new_node->key[j] = new_key[i*SLAB_SIZE+j]; 
+			new_node->val[j]= new_val[i*SLAB_SIZE+j];
+    			//SL->key[j] = new_key[i*SLAB_SIZE+j]; 
+			//SL->val[j]= new_val[i*SLAB_SIZE+j];
+			printf("key--->%d\tVal---->%d\n",new_node->key[j],new_node->val[j]);
+		}
+		new_node->next = head_ref; 
+    		//SL = SL->next;
+		//SL->next = head_ref; 
+	memcpy(SL,new_node, size * sizeof(struct SlabList));
+     		head_ref    = new_node;
+	//	memcpy(temp,new_node, size * sizeof(struct SlabList));
+		SL++;
+	}
+	//SL->next =NULL;
+		//SL =SL->next;	
+	//return head_ref;
+    	/*while (head_ref != NULL) { 
+		for(int i=0;i<SLAB_SIZE;i++){
+        		printf("Key: %d\tValue:%d\n",head_ref->key[i],head_ref->val[i]); 		
+		}
+  	     	head_ref = head_ref->next; 
+	}*/
+//	SL->next =NULL;
+	printf("here!!!\n");
+		/*while (SL != NULL) { 
+		for(int i=0;i<SLAB_SIZE;i++){
+        		printf("Key: %d\tValue:%d\n",SL->key[i],SL->val[i]); 		
+		}
+  	     	SL = SL->next; 
+		}*/
 }
-
-SlabList *createSlablist(void){
-	SlabList *slablist;
-	cudaMalloc(&slablist, sizeof(SlabList));
-	//Invoke a kernel with single thread to create a head node
-	create_head<<<1, 1>>>(slablist);
-	cudaDeviceSynchronize();
-	return slablist;
-}
-//We need to have two reads for synchronize
-__device__ void insertSlablist(SlabList *slablist, int ele){
-	Slab *new_node, *dest, *read1, *read2;
-  	int i, key=1; 
- 	while (key < MAX_VAL)
-    		key++;// Randomly assign keys
-	new_node = node_create(ele, key);
-  	for (i = 0; i < key; i++) {
-    		do {
-      			dest = node_search(slablist, ele, i); // want to insert right after this node
-      			read1 = dest->next[i];
-      			new_node->next[i] = read1;
-	//Ref: From stackoverflow for atomicCAS
-      			read2= (Slab *)atomicCAS((unsigned long long int *)&(dest->next[i]),
-        *(unsigned long long int *)&read1,
-        *(unsigned long long int *)&new_node);
-		} while (read1 != read2);
-  	}
-}
-__device__ Slab *node_create(int val, int key){
-	Slab *node= (Slab *)malloc(sizeof(Slab));
-	node->val = val;
-	node->key = key;
-	node->next = (Slab **)malloc(key * sizeof(Slab *));
-  	return node;
-}
-//Search for the element before the insertion
-__device__ Slab *node_search(SlabList *slablist, int ele, int search_key){
-	Slab *cur = slablist->head;
-	Slab *next_node;
-	int key, flag=0;
-	for (key = MAX_VAL - 1;key >= search_key; key--) {
-		next_node= cur->next[key];
-		while (next_node!= NULL && next_node->val < ele) {
-			if(!flag && blockIdx.x==0 && gridDim.x==32 && threadIdx.x==31){
-		//		printf("Inserting Elements!!"); 
-			printf("ele:%d\n",cur->next[key]->val);	
-			}
-		cur = next_node;
-      		next_node= cur->next[key];
-    		}
-		flag=1;
-  	}
-	if(blockIdx.x==0 && gridDim.x==32 && threadIdx.x==31)
- 	printf("_______________");
-  	return cur;
-}
-
-__global__ void insertKernel(SlabList *slablist, int *a, int N){
-	int thId = threadIdx.x + blockIdx.x * blockDim.x;
-	while (thId < N) {
-    		insertSlablist(slablist, a[thId]);
-    		thId += blockDim.x * gridDim.x;
-  	}
-}
+} 
 
 int main(void){
-	int N = 32;
-  	int *array = (int *)malloc(N * sizeof(int));
-  	int *device_array;
-  	int i;
-  	SlabList *slablist;
-  	cudaDeviceSetLimit(cudaLimitMallocHeapSize, 128*1024*1024);
-  	size_t limit;
-  	cudaDeviceGetLimit(&limit, cudaLimitMallocHeapSize);
-  	cudaMalloc(&device_array, N * sizeof(int));
-  	for (i = 0; i < N; i++)
-    		array[i] = i;
-  	printf("done initializing\n");
-  	slablist = createSlablist();
-  	cudaMemcpy(device_array, array, N * sizeof(int), cudaMemcpyHostToDevice);
- 	insertKernel<<<32, 32>>>(slablist, device_array, N);
-  	cudaDeviceSynchronize();
-  	printf("done inserting.\n");
-	return 0;
+	int N = 12;
+  	int *val_array = (int *)malloc(N * sizeof(int));
+  	int *key_array = (int *)malloc(N * sizeof(int));
+  	int *d_val_array = NULL;
+  	int *d_key_array = NULL;
+  	struct SlabList *start=(struct SlabList*)malloc(sizeof(struct SlabList));
+  	struct SlabList *d_start=NULL;
+	//HashTable = (struct Slab*)malloc(10*sizeof(struct Slab)); 
+  	cudaMalloc(&d_start, N * sizeof(struct SlabList));
+  	cudaMalloc(&d_val_array, N * sizeof(int));
+  	cudaMalloc(&d_key_array, N * sizeof(int));
+	for (int i = 0; i < N; i++){
+		val_array[i] =i;
+		key_array[i] =i+10;   
+	}
+	const clock_t begin_time = clock();
+	//Batch insertion
+	start = NULL;
+	struct SlabList* head = createSlablist(start, val_array,key_array,N);  	
+		//printf("%d\n",head->val[0]);
+	float runTime = (float)( clock() - begin_time ) /  CLOCKS_PER_SEC;
+        //printf("done initializing\n");
+        cudaMemcpy(d_val_array, val_array, N * sizeof(int), cudaMemcpyHostToDevice);
+        cudaDeviceSetLimit(cudaLimitMallocHeapSize, sizeof(struct SlabList)*N);
+        cudaMemcpy(d_key_array, key_array, N * sizeof(int), cudaMemcpyHostToDevice);
+	struct SlabList* d_SL = NULL;
+  	cudaMalloc(&d_SL, N * sizeof(struct SlabList));
+	struct SlabList* d_temp = NULL;
+  	cudaMalloc(&d_temp, N * sizeof(struct SlabList));	
+        const clock_t begin_time1 = clock();
+        insertKernel<<<1, 1>>>(d_start,d_val_array,d_key_array,N,d_SL,d_temp);
+//        insertKernel<<<1, 1>>>(d_SL);
+//       cudaDeviceSynchronize();
+        struct SlabList* head1 = (struct SlabList*)malloc(N*sizeof(struct SlabList));
+        cudaMemcpy(head1, d_SL, N * sizeof(struct SlabList), cudaMemcpyDeviceToHost);
+	//printf("%d\n",head1->key[0]);
+        float runTime1 = (float)( clock() - begin_time1 ) /  CLOCKS_PER_SEC;
+        printf("Time for matching keywords: %fs\n\n", runTime1);
+        printf("____________________________GPU Insertion!!!_______________________________________\n");
+        printList1(head1,N/SLAB_SIZE);
+	printf("Insert number of elements you want to insert\n");
+	int num;
+	scanf("%d",&num);
+	printf("Enter key and value pair \n");
+	for(int i=0;i<num;i++)
+		scanf("%d%d",&key_array[i],&val_array[i]);
+	printf("Entered key values are\n");
+	for(int i=0;i<num;i++)
+		printf("Key:%d\tVal:%d\n",key_array[i],val_array[i]);
+return 0;
 }
